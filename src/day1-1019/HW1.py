7fcb151c75813933e1f433241c070b92923dbff6
mryyomutga <mryyomutga@gmail.com>
2017-10-24 13:06:53 +0900

    5th commit
    プログラムの修正

--- src/day1-1019/HW1.py
+++ src/day1-1019/HW1.py
@@ -1,9 +1,9 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 # 2017/10/19(木) 専門ゼミ 第1回 課題
 # Author : 宮本 涼雅
 # Stu.ID : 1536388
 # class  : 3EP2-46
-# Date : 2017/10/20(金)
+# Date   : 2017/10/20(金)
 
 from __future__ import print_function
 import time
@@ -13,11 +13,11 @@ import time
 face = ("(9_9)", "(6_6)")
 
 for n in range(0, 10):
+    time.sleep(0.5)
     if n % 2 == 0:
         print(face[0])
     else:
         print(face[1])
-    time.sleep(0.5)
 
 # 所要時間 : 3分
 
@@ -26,14 +26,14 @@ for n in range(0, 10):
 
 start = time.time() # 計測開始時刻の取得
 now_time = start
-n = 0               # インクリメント用変数
+cnt = 0               # インクリメント用変数
 
 # 開始時刻と現在の時間の差が5.0秒以下であればインクリメントし続ける
 while now_time - start < 5.0:
     now_time = time.time();  # 現在の時刻の取得
-    n += 1
+    cnt += 1
     time.sleep(0.1)      # 0.1秒スリープ
 
-print(n)
+print(cnt)
 
 # 所要時間 : 20分
